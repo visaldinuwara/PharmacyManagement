04b7332505eaa10d56949f8c2bc23f14ca75408f
Visal Dinuwara <visaldinuwara@gmail.com>
2025-11-29 23:51:43 +0530

    Model

---
+++ src/main/java/Controller/LoginPageController.java
@@ -0,0 +1,29 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginPageController {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Label messageLabel;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    void handleLoginButtonAction(ActionEvent event) {
+
+    }
+
+}
