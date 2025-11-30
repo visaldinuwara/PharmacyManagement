771ca10520fefc2237cfbd7e9b224d7873169c6f
Visal Dinuwara <visaldinuwara@gmail.com>
2025-11-30 10:36:10 +0530

    Model

--- src/main/java/Model/DTO/LoginDetailsDTO.java
+++ src/main/java/Model/DTO/LoginDetailsDTO.java
@@ -1,5 +1,12 @@
 package Model.DTO;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Setter
+@Getter
 public class LoginDetailsDTO {
     private String userName;
     private String password;
--- src/main/java/Model/Entity/LoginDetails.java
+++ src/main/java/Model/Entity/LoginDetails.java
@@ -1,5 +1,12 @@
 package Model.Entity;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class LoginDetails {
     private String userName;
     private String password;
