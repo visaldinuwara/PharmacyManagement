c1bb82d93b11a02469ceda030a47a67b3bd33345
Visal Dinuwara <visaldinuwara@gmail.com>
2025-12-17 00:08:52 +0530

    Controller Corrected

--- src/main/java/Controller/LoginPageController.java
+++ src/main/java/Controller/LoginPageController.java
@@ -1,14 +1,23 @@
 package Controller;
 
+import Service.LoginPageService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 public class LoginPageController {
 
+    private LoginPageService loginPageService;
     @FXML
     private Button loginButton;
 
@@ -22,8 +31,13 @@ public class LoginPageController {
     private TextField usernameField;
 
     @FXML
-    void handleLoginButtonAction(ActionEvent event) {
-
+    void handleLoginButtonAction(ActionEvent event) throws SQLException, IOException {
+        //if(loginPageService.loginCheck(usernameField.getText()).equals(passwordField.getText())){
+            Parent root= FXMLLoader.load(getClass().getResource("/View/Dashboard.fxml"));
+            Stage stage=new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        //}
     }
 
 }
